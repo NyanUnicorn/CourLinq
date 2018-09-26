0800967b551f2d1f0f5c39b6c79b006bd3ea651f
Phillip Redfern <philllip347@gmail.com>
2018-09-26 16:55:55 +0200

    updt

--- CourLinq/Program.cs
+++ CourLinq/Program.cs
@@ -4,12 +4,168 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CourLinq
 {
     class Program
     {
+
+        
         static void Main(string[] args)
         {
+            List<Vehicule> vehicules = Vehicule.GetVehicules(10);
+            /*
+            foreach (Vehicule voiture in Vehicule.GetVehicules(10))
+            {
+                Console.WriteLine($"{voiture.Marque} {voiture.Model} -- {voiture.Color} \n     places : {voiture.PlaceNum} \n     portes : {voiture.DoorNum} \n     kilometrage : {voiture.KilometerCount} \n     Plaque : {voiture.LicensePalte}" +
+                    $"\n     PS:{voiture.MotorPower}");
+            }
+            */
+            bool running = true;
+            while (running)
+            {
+                Console.Clear();
+                Console.WriteLine("1 - Afficher la liste complète");
+                Console.WriteLine("2 - Afficher les voitures de marque...");
+                Console.WriteLine("3 - Afficher les vehicules de la marque triées par km");
+                Console.WriteLine("4 - Afficher le nombre de vehicules d'une marque donnée");
+                Console.WriteLine("5 - Demander 2 Dates et afficher les vehicules entrés pendant période");
+                Console.WriteLine("6 - Afficher le vehicule qui à le plus grand kilométrage");
+                Console.WriteLine("7 - Afficher la moyenne du kilométrage des véhicules du parc");
+                Console.WriteLine("8 - Afficher la moyenne du prix total / J des véhicules du parc");
+                Console.WriteLine("9 - Afficher pour chaque marque le nombre de véhicules (trié par marque");
+                Console.WriteLine("0 - Quitter");
+
+                switch (readChoice())
+                {
+                    case "1":
+                        displayVehicles(vehicules);
+                        break;
+                    case "2":
+                        displayVehiclesOfBrand(vehicules);
+                        break;
+                    case "3":
+                        displayVehiclesOfBrand(vehicules, 1);
+                        break;
+                    case "4":
+                        displayVehiclesOfBrand(vehicules, 2);
+                        break;
+                    case "5":
+
+                        break;
+                    case "6":
+                        displayVehiclesOfBrand(vehicules, 3);
+                        break;
+                    case "7":
+                        displayVehiclesOfBrand(vehicules, 4);
+                        break;
+                    case "8":
+                        displayVehiclesOfBrand(vehicules, 5);
+                        break;
+                    case "9":
+                        displayVehiclesOfBrand(vehicules, 6);
+                        break;
+                    case "0":
+                        running = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Demande une entré pour le nom de la marque puis affiche une liste de vehicules de la marque demandé
+        ///     0 = no mod;
+        ///     1 = order by km count;
+        ///     2 = number of vehicles;
+        ///     3 = average of vehicle kilometer count in park
+        ///     4 = kilométrage moyen
+        ///     5 = prix moyen par jour
+        ///     6 = nombre de voitur trié par marque
+        /// </summary>
+        /// <param name="_vehiculeList"></param>
+        /// <param name="mod"></param>
+        static void displayVehiclesOfBrand(List<Vehicule> _vehiculeList, int mod = 0)
+        {
+            Console.Clear();
+            
+            string marque;
+            switch (mod)
+            {
+                case 0:
+                default:
+                    Console.WriteLine("Entrez le nom de la marque que vous cherchez");
+                    marque = Console.ReadLine();
+                    displayVehicles(_vehiculeList.Where((voiture) => voiture.Marque.ToLower().Contains(marque)).ToList());
+                    break;
+                case 1:
+                    Console.WriteLine("Entrez le nom de la marque que vous cherchez");
+                    marque = Console.ReadLine();
+                    displayVehicles(_vehiculeList.Where((voiture) => voiture.Marque.ToLower().Contains(marque)).OrderBy((voiture) => voiture.KilometerCount).ToList());
+                    break;
+                case 2:
+                    Console.WriteLine("Entrez le nom de la marque que vous cherchez");
+                    marque = Console.ReadLine();
+                    Console.WriteLine($"{_vehiculeList.Where((voiture) => voiture.Marque.ToLower().Contains(marque)).Count()}");
+                    Console.ReadKey();
+                    break;
+                case 3:
+                    Console.WriteLine("Le vehicule avec le plus grand kilométrage est : ");
+                    displayVehicles(_vehiculeList.Where((voiture) => voiture.KilometerCount == _vehiculeList.Max(v => v.KilometerCount)).ToList());
+                    break;
+                case 4:
+                    Console.WriteLine($"Moyenne Kilométrage : {_vehiculeList.Average(v => v.KilometerCount)}");
+                    break;
+                case 5:
+                    Console.WriteLine($"Moyenne Prix par jour : {_vehiculeList.Average(v => v.RangeCoef)}");
+                    break;
+                case 6:
+                    List<Vehicule> veh = new List<Vehicule>();
+
+                    _vehiculeList.GroupBy(v => v.Marque).OrderBy(v => v.Key).ToList().ForEach(grp => Console.WriteLine($"{grp.Key}  {grp.Count()}"));
+                   
+                    Console.ReadKey();
+                    break;
+                
+            }
+        }
+
+        /// <summary>
+        ///     Affiche une liste de vahicules d'une liste donnée
+        /// </summary>
+        /// <param name="_vehiculeList"></param>
+        static void displayVehicles(List<Vehicule> _vehiculeList)
+        {
+            Console.Clear();
+            if (_vehiculeList.Count() > 0)
+            {
+                foreach (Vehicule voiture in _vehiculeList)
+                {
+                    displayVehicle(voiture);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Pas de vehicule de cette marque");
+            }
+            Console.ReadKey();
+        }
+
+        static void displayVehicle(Vehicule _v)
+        {
+            Console.WriteLine($"{_v.Marque} {_v.Model} -- {_v.Color} \n     places : {_v.PlaceNum} \n     portes : {_v.DoorNum} \n     kilometrage : {_v.KilometerCount} \n     Plaque : {_v.LicensePalte}" +
+                       $"\n     PS:{_v.MotorPower}");
+        }
+
+
+        /// <summary>
+        ///     Retourne la touche qui à été touché
+        /// </summary>
+        /// <returns>string</returns>
+        private static string readChoice()
+        {
+            return Console.ReadKey().KeyChar.ToString();
         }
     }
 }
---
+++ CourLinq/Vehicule.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourLinq
+{
+    class Vehicule
+    {
+        #region Static Fields
+        private static Random rand = new Random();
+        private static string[] colorList = new string[] { "Jaune", "Bleu", "Vert", "Turquoise", "Cerise" , "Rouge", "Gris Spaciale", "Or", "Maron", "Argent", "Mauve", "Orange", "Brique", "Poussin", "Taupe", "Prune"};
+        public static string[] brandList = new string[] { "Renault", "Lamborghini", "Hyundai", "Toyota" };
+        private static string[] modelListRenault = new string[] { "Twingo", "Clio", "Megane" };
+        private static string[] modelListLamborghini = new string[] { "Aventador", "Diablo", "Hurakan" };
+        private static string[] modelListHyundai = new string[] { "H30", "H50", "Coupé" };
+        private static string[] modelListToyota = new string[] { "Corola", "Supra", "Aygo" };
+        private static int[] doorNumbList = new int[] { 3, 5 };
+        private static int[] placeNumbList = new int[] { 2, 5 };
+        private static string[] rentalRangeList = new string[] { "Eco", "Standard", "Luxe" };
+
+        #endregion
+        #region Fields
+
+        /// <summary>
+        ///     Plaque d'immatriculation du véhicule
+        /// </summary>
+        private string licensePalte;
+        /// <summary>
+        ///     Couleur du véhicule
+        /// </summary>
+        private string color;
+        /// <summary>
+        ///     Kilométrage: nombre de kilomètre parcourue par le véhicule
+        /// </summary>
+        private int kilometerCount;
+        /// <summary>
+        ///     Puissance du moteur du véhicule
+        /// </summary>
+        private int motorPower;
+        /// <summary>
+        ///     marque du véhicule
+        /// </summary>
+        private string marque;
+        /// <summary>
+        ///     model du véhicule
+        /// </summary>
+        private string model;
+        /// <summary>
+        ///     date d'arrivée du véhicule dans le parc
+        /// </summary>
+        private DateTime arrivalDateTime;
+        /// <summary>
+        ///     Nombre de porte sur le véhicule
+        /// </summary>
+        private int doorNum;
+        /// <summary>
+        ///     Nombre de places assise dans le véhicule
+        /// </summary>
+        private int placeNum;
+        /// <summary>
+        ///     gamme du véhicule (eco(0,8), luxe(1,0), normale(1,2))
+        /// </summary>
+        private string rentalRange;
+        /// <summary>
+        ///     Prix location à journée du véhicule
+        /// </summary>
+        private double rentalBasePricePerDay;
+        /// <summary>
+        ///     Prix réel à la journée (prix loc * coef de la gamme)
+        /// </summary>
+        private double rangeCoef;
+
+        #endregion
+
+
+        #region Properties
+        /// <summary>
+        ///     Obtient ou définit la plaque d'immatriculation du véhicule
+        /// </summary>
+        public string LicensePalte { get => licensePalte; set => licensePalte = value; }
+        /// <summary>
+        ///     Obtient ou définit la couleur du véhicule
+        /// </summary>
+        public string Color { get => color; set => color = value; }
+        /// <summary>
+        ///     Obtient ou définit le nombre de kilometre parcouru par le véhicule
+        /// </summary>
+        public int KilometerCount { get => kilometerCount; set => kilometerCount = value; }
+        /// <summary>
+        ///     Obtient ou définit la puissance du moteur du véhicule
+        /// </summary>
+        public int MotorPower { get => motorPower; set => motorPower = value; }
+        /// <summary>
+        ///     Obtient ou définit la marque du véhicule
+        /// </summary>
+        public string Marque { get => marque; set => marque = value; }
+        /// <summary>
+        ///     Obtient ou définit le modele du véhicule
+        /// </summary>
+        public string Model { get => model; set => model = value; }
+        /// <summary>
+        ///     Obtient ou définit la date d'arrivée du véhicule dans le parc
+        /// </summary>
+        public DateTime ArrivalDateTime { get => arrivalDateTime; set => arrivalDateTime = value; }
+        /// <summary>
+        ///     Obtient ou définit le nombre de porte sur le véhicule
+        /// </summary>
+        public int DoorNum { get => doorNum; set => doorNum = value; }
+        /// <summary>
+        ///     Obtient ou définit le nombre de place dans le véhicule
+        /// </summary>
+        public int PlaceNum { get => placeNum; set => placeNum = value; }
+        /// <summary>
+        ///     Obtient ou définit la gamme du véhicule
+        /// </summary>
+        public string RentalRange { get => rentalRange; set => rentalRange = value; }
+        /// <summary>
+        ///     Obtient ou définit le prix location à journée
+        /// </summary>
+        public double RentalBasePricePerDay { get => rentalBasePricePerDay; set => rentalBasePricePerDay = value; }
+        /// <summary>
+        ///     Obtient ou définit le prix réel à la journée (prix loc * coef de la gamme)
+        /// </summary>
+        public double RangeCoef {
+            get
+            {
+                switch (rentalRange)
+                {
+                    case "Eco": return 0.80;
+                    case "Standard": default: return 1;
+                    case "Luxe": return 1.20;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public static List<Vehicule> GetVehicules(int _vehiculeCount)
+        {
+            List<Vehicule> result = new List<Vehicule>();
+            for(int i = 0; i < _vehiculeCount; i++)
+            {
+                Vehicule vehicule = new Vehicule(true);
+                result.Add(vehicule);
+            }
+
+            return result;
+        }
+
+
+        #endregion
+
+        #region Constructors
+
+        public Vehicule()
+        {
+
+        }
+
+        public Vehicule(bool _generateRand = false)
+        {
+            
+            if (_generateRand)
+            {
+                this.licensePalte = Guid.NewGuid().ToString();
+                this.color = colorList[rand.Next(0, colorList.Length)];
+                this.kilometerCount = rand.Next(100, 150000);
+                this.motorPower = rand.Next(7, 40) * 10;
+                int brandId = rand.Next(0, brandList.Length);
+                this.marque = brandList[brandId];
+                switch (brandId)
+                {
+                    case 0:
+                    default:
+                        this.model = modelListRenault[rand.Next(0, modelListRenault.Length)];
+                        break;
+                    case 1:
+                        this.model = modelListLamborghini[rand.Next(0, modelListLamborghini.Length)];
+                        break;
+                    case 2:
+                        this.model = modelListHyundai[rand.Next(0, modelListHyundai.Length)];
+                        break;
+                    case 3:
+                        this.model = modelListToyota[rand.Next(0, modelListToyota.Length)];
+                        break;
+                }
+                this.arrivalDateTime = DateTime.Now.Date.AddDays(rand.Next(-700, 0));
+                this.doorNum = doorNumbList[rand.Next(0, doorNumbList.Length)];
+                this.placeNum = placeNumbList[rand.Next(0, doorNumbList.Length)];
+                this.rentalRange = rentalRangeList[rand.Next(0, rentalRangeList.Length)];
+                this.rentalBasePricePerDay = rand.Next(10, 15);
+              
+                
+
+
+            }
+
+            
+        }
+
+
+        #endregion
+
+
+        /**
+         * Créer une classe qui est déterminée par : 
+         * Immatriculation (Chaine)
+         * Couleur(chaine)
+         * Kilométrage(entier)
+         * Puissance moteur (entier)
+         * Marque (chaine)
+         * Modèle (cahine)
+         * Date d'entrée dans le parc (DateTime)
+         * Nombre de porte (entier)
+         * Nombre de place (entier)
+         * Gamme (éco(0.8), luxe(1), standard(1.2)) (chaine)
+         * Prix location à journée (double)
+         * Prix réel à la journée (prix loc * coef de la gamme) (double)
+         * 
+         * */
+    }
+}
